0725f099c01158da15e677635b888a4ca705dca7
Willian Nascimento <willianjoga6@hotmail.com>
2020-02-29 17:38:36 -0300

    ok

--- sssh.py
+++ sssh.py
@@ -1,6 +1,20 @@
 import paramiko
+
+
 try:
     client = paramiko.client.SSHClient()
     client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    client.connect('127.0.0.1')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect('192.168.200.111',
+                   username='developer',
+                   password='4linux',
+                   port='22')
+except Exception as e:
+    print('Erro conexão: {}'.format(e))
+    exit()
+
+stdin, stdout, stderr = client.exec_command('sudo init 0')
+if stdout.channel.recv_exit_status() == 0:
+    print(stdout.read().decode('utf-8'))
+else:
+    print(stderr.read().decode('utf-8'))
